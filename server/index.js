55dda49239a475d1a7acf6d8f4d07786979f6d31
Shashwat Dixit <dixitsumit117@gmail.com>
2024-02-22 11:16:17 +0530

    My database conected

--- server/index.js
+++ server/index.js
@@ -25,6 +25,7 @@ app.use("/api/messages", messageRoutes);
 const server = app.listen(process.env.PORT, () =>
   console.log(`Server started on ${process.env.PORT}`)
 );
+
 const io = socket(server, {
   cors: {
     origin: "http://localhost:3000",
